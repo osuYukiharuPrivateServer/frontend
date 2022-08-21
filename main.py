216cdd9b3f9d8124adbb8e387c4457a3da953531
Devin Yang <hedgehog_qd@163.com>
2022-08-21 14:47:58 +0800

    changed colour

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from objects import glob
 
 app = Quart(__name__)
 
-version = Version(1, 3, 0)
+version = Version(2, 1, 0)
 
 # used to secure session data.
 # we recommend using a long randomly generated ascii string.
